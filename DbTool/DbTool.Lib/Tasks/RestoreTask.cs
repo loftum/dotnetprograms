4e2db8c2ebdca29873f966cb39a6638dfddb9a96
loftum <loftum@gmail.com>
2011-09-20 16:45:31 +0200

    Added RestoreTask

---
+++ DbTool/DbTool.Lib/Tasks/RestoreTask.cs
@@ -0,0 +1,97 @@
+﻿using System.IO;
+using System.Text;
+using DbTool.Lib.Configuration;
+using DbTool.Lib.Logging;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace DbTool.Lib.Tasks
+{
+    public class RestoreTask
+    {
+        public event PercentCompleteEventHandler PercentComplete;
+        public event ServerMessageEventHandler Complete;
+
+        private readonly IDbToolLogger _logger;
+        private readonly IDbToolSettings _settings;
+
+        public RestoreTask(IDbToolLogger logger, IDbToolSettings settings)
+        {
+            _settings = settings;
+            _logger = logger;
+        }
+
+        public void Restore(BackupParameters parameters)
+        {
+            var databaseName = parameters.DatabaseName;
+            var filePath = GetFullPathFrom(parameters.FilePath);
+
+            _logger.WriteLine("Restoring " + databaseName + " from file " + filePath);
+            var server = new Server(parameters.Server);
+            try
+            {
+                server.ConnectionContext.LoginSecure = true;
+                server.ConnectionContext.Connect();
+                var restore = new Restore { Database = databaseName, Action = RestoreActionType.Database };
+                restore.Devices.AddDevice(filePath, DeviceType.File);
+                restore.ReplaceDatabase = true;
+
+                var database = server.Databases[databaseName];
+                if (database != null)
+                {
+                    for (var ii = 0; ii < database.LogFiles.Count; ii++)
+                    {
+                        var logFile = database.LogFiles[ii];
+                        _logger.WriteLine("Logfile: " + logFile);
+                        var physicalFilename = new StringBuilder().Append(databaseName).Append("_log.ldf").ToString();
+                        var physicalPath = Path.Combine(_settings.LogDirectory, physicalFilename);
+                        restore.RelocateFiles.Add(new RelocateFile(logFile.Name, physicalPath));
+                        _logger.WriteLine("Relocating log file [" + logFile.Name + "] to " + physicalPath);
+                    }
+
+                    for (var ii = 0; ii < database.FileGroups.Count; ii++)
+                    {
+                        var group = database.FileGroups[ii];
+                        _logger.WriteLine("Group: " + group.Name);
+                        for (var jj = 0; jj < group.Files.Count; jj++)
+                        {
+                            var file = group.Files[jj];
+                            _logger.WriteLine("Name: " + file.Name);
+                            _logger.WriteLine("IsPrimary: " + file.IsPrimaryFile);
+                            _logger.WriteLine("FileName: " + file.FileName);
+                            if (file.IsPrimaryFile)
+                            {
+                                var physicalFilename = new StringBuilder().Append(databaseName).Append(".mdf").ToString();
+                                var physicalPath = Path.Combine(_settings.DataDirectory, physicalFilename);
+                                restore.RelocateFiles.Add(new RelocateFile(file.Name, physicalPath));
+                                _logger.WriteLine("Relocating data file [" + file.Name + "] to " + physicalPath);
+                            }
+                        }
+                    }
+                    server.ConnectionContext.Disconnect();
+                    server.ConnectionContext.Connect();
+                }
+
+                restore.PercentComplete += PercentComplete;
+                restore.Complete += Complete;
+                restore.SqlRestore(server);
+            }
+            finally
+            {
+                if (server.ConnectionContext.IsOpen)
+                {
+                    _logger.Write("Closing connection...");
+                    server.ConnectionContext.Disconnect();
+                    _logger.WriteLine("OK");
+                }
+            }
+        }
+
+        private string GetFullPathFrom(string filePath)
+        {
+            return Path.IsPathRooted(filePath)
+                ? filePath
+                : Path.Combine(_settings.BackupDirectory, filePath);
+        }
+    }
+}
--- DbTool/DbTool/Tasks/DatabaseRestorer.cs
+++ DbTool/DbTool/Tasks/DatabaseRestorer.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using DbTool.Lib.Configuration;
 using DbTool.Lib.Logging;
-using Microsoft.SqlServer.Management.Smo;
+using DbTool.Lib.Tasks;
 
 namespace DbTool.Tasks
 {
@@ -23,68 +21,18 @@ namespace DbTool.Tasks
 
         public override void DoExecute(IList<string> args)
         {
-            var databaseName = args[1];
-            var filePath = args[2];
-
-            Logger.WriteLine("Restoring " + databaseName + " from file " + filePath);
-            var server = new Server("(local)");
-            try
-            {
-                server.ConnectionContext.LoginSecure = true;
-                server.ConnectionContext.Connect();
-                var restore = new Restore {Database = databaseName, Action = RestoreActionType.Database};
-                restore.Devices.AddDevice(filePath, DeviceType.File);
-                restore.ReplaceDatabase = true;
-
-                var database = server.Databases[databaseName];
-                if (database != null)
+            var connectionData = Settings.GetConnection(args[1]);
+            var parameters = new BackupParameters
                 {
-                    for (var ii = 0; ii < database.LogFiles.Count; ii++)
-                    {
-                        var logFile = database.LogFiles[ii];
-                        Logger.WriteLine("Logfile: " + logFile);
-                        var physicalFilename = new StringBuilder().Append(databaseName).Append("_log.ldf").ToString();
-                        var physicalPath = Path.Combine(Settings.LogDirectory, physicalFilename);
-                        restore.RelocateFiles.Add(new RelocateFile(logFile.Name, physicalPath));
-                        Logger.WriteLine("Relocating log file [" + logFile.Name + "] to " + physicalPath);
-                    }
+                    DatabaseName = connectionData.Name,
+                    FilePath = args[2],
+                    Server = connectionData.Host
+                };
 
-                    for (var ii = 0; ii < database.FileGroups.Count; ii++)
-                    {
-                        var group = database.FileGroups[ii];
-                        Logger.WriteLine("Group: " + group.Name);
-                        for (var jj = 0; jj < group.Files.Count; jj++)
-                        {
-                            var file = group.Files[jj];
-                            Logger.WriteLine("Name: " + file.Name);
-                            Logger.WriteLine("IsPrimary: " + file.IsPrimaryFile);
-                            Logger.WriteLine("FileName: " + file.FileName);
-                            if (file.IsPrimaryFile)
-                            {
-                                var physicalFilename = new StringBuilder().Append(databaseName).Append(".mdf").ToString();
-                                var physicalPath = Path.Combine(Settings.DataDirectory, physicalFilename);
-                                restore.RelocateFiles.Add(new RelocateFile(file.Name, physicalPath));
-                                Logger.WriteLine("Relocating data file [" + file.Name + "] to " + physicalPath);
-                            }
-                        }
-                    }
-                    server.ConnectionContext.Disconnect();
-                    server.ConnectionContext.Connect();
-                }
-
-                restore.PercentComplete += PrintPercentage;
-                restore.Complete += TaskComplete;
-                restore.SqlRestore(server);
-            }
-            finally
-            {
-                if (server.ConnectionContext.IsOpen)
-                {
-                    Logger.Write("Closing connection...");
-                    server.ConnectionContext.Disconnect();
-                    Logger.WriteLine("OK");
-                }
-            }
+            var restoreTask = new RestoreTask(Logger, Settings);
+            restoreTask.PercentComplete += PrintPercentage;
+            restoreTask.Complete += TaskComplete;
+            restoreTask.Restore(parameters);
         }
     }
 }
