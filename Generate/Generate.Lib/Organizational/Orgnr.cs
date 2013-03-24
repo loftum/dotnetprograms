60209fb3e82c1e2c583be5c25f390eaa37deabd4
loftum <loftum@gmail.com>
2013-03-24 08:08:57 +0100

    Orgnr

--- Generate/Generate.Lib/Organizational/Orgnr.cs
+++ Generate/Generate.Lib/Organizational/Orgnr.cs
@@ -4,13 +4,22 @@ namespace Generate.Lib
 {
 	public class Orgnr : LegalNumber
 	{
+		public OrgnrNumbers Numbers { get; private set; }
+
 		public Orgnr (string value) : base(value)
 		{
+			Numbers = new OrgnrNumbers(value);
 		}
 
 		public override void Validate()
 		{
-			throw new NotImplementedException();
+			var sum = 3 * Numbers.S1 + 2 * Numbers.S2 + 7 * Numbers.S3 + 6 * Numbers.S4 + 5 * Numbers.S5 + 4 * Numbers.S6 + 3 * Numbers.S7 + 2 * Numbers.S8;
+			var rest = sum % 11;
+			var k = rest == 0 ? 0 : 11 - rest;
+			if (k == 10 || k != Numbers.K)
+			{
+				throw new Exception("Invalid K");
+			}
 		}
 	}
 }
---
+++ Generate/Generate.Lib/Organizational/OrgnrNumbers.cs
@@ -0,0 +1,44 @@
+using System;
+using DotNetPrograms.Common.ExtensionMethods;
+
+namespace Generate.Lib
+{
+	public class OrgnrNumbers
+	{
+		public int S1 { get; private set; }
+		public int S2 { get; private set; }
+		public int S3 { get; private set; }
+		public int S4 { get; private set; }
+		public int S5 { get; private set; }
+		public int S6 { get; private set; }
+		public int S7 { get; private set; }
+		public int S8 { get; private set; }
+		public int K { get; private set; }
+
+		public OrgnrNumbers (string value)
+		{
+			S1 = GetOrDefault<int> (value, 0);
+			S2 = GetOrDefault<int> (value, 1);
+			S3 = GetOrDefault<int> (value, 2);
+			S4 = GetOrDefault<int> (value, 3);
+			S5 = GetOrDefault<int> (value, 4);
+			S6 = GetOrDefault<int> (value, 5);
+			S7 = GetOrDefault<int> (value, 6);
+			S8 = GetOrDefault<int> (value, 7);
+			K = GetOrDefault<int> (value, 8);
+		}
+
+		private static T GetOrDefault<T>(string value, int index, int length = 1)
+		{
+			try
+			{
+				return value.Substring(index, length).ConvertTo<T>();
+			}
+			catch
+			{
+				return default(T);
+			}
+		}
+	}
+}
+
